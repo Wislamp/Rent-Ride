f5b98358d77b1af169d3f93d6d41acce4ebf8fbf
Solomon Alan-Dei <alandei.solomon@gmail.com>
2024-02-22 00:10:28 +0000

    add region table to database

--- models.py
+++ models.py
@@ -26,8 +26,7 @@ class Rental(db.Model):
     __tablename__ = 'rentals'
 
     id = db.Column(db.Integer, primary_key=True)
-    pickup_date = db.Column(db.DateTime)
-    return_date = db.Column(db.DateTime)
+    duration = db.Column(db.Integer)
 
     # Foreign Keys
     car_id = db.Column(db.Integer, db.ForeignKey('cars.id'))
@@ -47,9 +46,21 @@ class Customer(db.Model):
     email = db.Column(db.String(60), unique=True)
     phone = db.Column(db.String(15))
     address = db.Column(db.String(200))
+    city = db.Column(db.String(100))
 
     # Relationships
+    region = db.Relationship('Region', backref='region', lazy='dynamic')
     rentals = db.relationship('Rental', backref='customer', lazy='dynamic')
+
+class Region(db.Model):
+    """
+    Create a Region table
+    """"
+
+    __tablename__ = 'regions'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
 
 def db_create(app):
     with app.app_context():
