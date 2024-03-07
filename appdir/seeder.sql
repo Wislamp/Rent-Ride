4859a2e41bc8b79c2ea1f2379c8e5e103000a2cb
Solomon Alan-Dei <alandei.solomon@gmail.com>
2024-03-07 08:44:46 +0000

    seed cars and remove images not needed

--- appdir/seeder.sql
+++ appdir/seeder.sql
@@ -9,3 +9,6 @@ INSERT INTO regions (region) VALUES('Upper West');
 
 -- SEED CARS 
 INSERT INTO cars (images, model, model_year, color, rate, rent_status, category) VALUES('orange-audi.jpg', 'Audi', 2020, 'Orange', 30, 'Available', 'Popular');
+INSERT INTO cars (images, model, model_year, color, rate, rent_status, category) VALUES('blue-bmw-exclusive.jpg', 'BMW', 2020, 'Blue', 50, 'Available', 'Popular');
+INSERT INTO cars (images, model, model_year, color, rate, rent_status, category) VALUES('red-nissan.jpg', 'Nissan', 2020, 'Red', 50, 'Available', 'Popular');
+INSERT INTO cars (images, model, model_year, color, rate, rent_status, category) VALUES('red-volvo.jpg', 'Volvo', 2020, 'Red', 70, 'Available', 'Popular');
